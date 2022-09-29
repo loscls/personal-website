38ffd17b2bb498fe0627b379d9e04a799f7808a1
simonpaolo <simonelopez@hotmail.it>
2022-09-29 02:31:15 +0200

    start page

--- angular-web-site/src/app/app.component.ts
+++ angular-web-site/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import lollo_cv from './_files/lollo_cv.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-web-site';
+  cv:any=lollo_cv;
+  stri:String=JSON.stringify(this.cv);
 }
