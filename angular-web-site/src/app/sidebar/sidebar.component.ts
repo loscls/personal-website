ccc34e1bb87c37ae59e19ee3fae484dd3c4b9580
simonpaolo <simonelopez@hotmail.it>
2022-10-05 03:17:27 +0200

    (add)open animation

--- angular-web-site/src/app/sidebar/sidebar.component.ts
+++ angular-web-site/src/app/sidebar/sidebar.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
-
+  status:string="close";
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  //semplicemente cambio la variabile dell'ngclass quindi assegno la classe open di cui ho definito il css
+  openCloseEvent():void{
+    if(this.status=="close"){
+      this.status="open";
+    }
   }
 
 }
