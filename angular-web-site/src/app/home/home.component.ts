654521ecbcb934bee46ca62375391d73e9e2dbaa
simonpaolo <simonelopez@hotmail.it>
2022-11-03 01:15:30 +0100

    fix(data)

--- angular-web-site/src/app/home/home.component.ts
+++ angular-web-site/src/app/home/home.component.ts
@@ -1,13 +1,16 @@
 import { Component, Input, OnInit } from '@angular/core';
-
+import lollo_cv from '../_files/lollo_cv.json';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  @Input() datiCv: any;
-
+  //@Input() datiCv: any; togliere ciò perché non li stiamo passando da padre ma leggendo da json
+  mostra:boolean=true;
+  title = 'angular-web-site';
+  datiCv:any = lollo_cv;
+  stri: String = JSON.stringify(this.datiCv);
   constructor() { }
 
   ngOnInit(): void {
